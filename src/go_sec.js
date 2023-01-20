0cbcda1b26b8bae5d269aefc92e6a5d77d04d126
Gyana Ranjan Panigrahi <gyanarp@jfrog.com>
2023-01-20 10:32:27 +0530

    "Pipeline TASK for running Go Sec"

--- src/go_sec.js
+++ src/go_sec.js
@@ -5,7 +5,7 @@ async function go_sec() {
   tasks.info("Checking if go installed for running GO Sec")
   const goPath = (await tasks.execute("go env GOPATH")).stdOut;
   if (goPath) {
-    tasks.info("Go is installed hence proceeding with the GO Sec")
+    tasks.info  ("Go is installed hence proceeding with the GO Sec")
     const goPathBin = path.join(goPath, "bin");
     tasks.appendToPath(goPathBin);
     tasks.info("Installing the GO Sec")
@@ -16,11 +16,8 @@ async function go_sec() {
 
     const abc = await tasks.execute(`curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s -- -b ${goPathBin}`)
     if (abc) {
-      let commandOutput = (await tasks.execute(`${goPathBin}`)).stdOut;
-      tasks.info("commandOutput:" +commandOutput)
       let commandOutput1 = (await tasks.execute(`ls -lrt`)).stdOut;
       tasks.info("commandOutput1111:" +commandOutput1)
-
       tasks.info("GO Sec installed successfully")
       tasks.info("Executing GO sec")
       const goSecRun = ((await tasks.execute(`${goPathBin}/gosec`)).stdOut);
