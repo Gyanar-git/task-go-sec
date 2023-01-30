e8963cf46fa5826a87f1aa6898a530fe3e65e201
Gyana Ranjan Panigrahi <gyanarp@jfrog.com>
2023-01-30 14:39:13 +0530

    "Run go sec check"

--- src/go_sec.js
+++ src/go_sec.js
@@ -70,7 +70,7 @@ async function installGoSec(goPath) {
   tasks.appendToPath(goPathBin);
   tasks.info("goPathBin: " + goPathBin);
   //tasks.execute(`cd ${goPathBin} `)
-  const {stdOut,stdErr} = (await tasks.execute(`rm -f ${goPathBin}/gosec && curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s -- -b ${goPathBin}`));
+  const {stdOut,stdErr} = (await tasks.execute(`go install github.com/securego/gosec/v2/cmd/gosec@latest`));
   //tasks.execute(`ls -lrt `)
   const err = JSON.stringify(stdErr);
   if (stdErr) {
